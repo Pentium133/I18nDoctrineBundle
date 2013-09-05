e36eb2ebc613c48df8abcd7d20cfe6ae1e2af221
Serg <github@puhoff.com>
2013-09-05 13:26:05 +0400

    enable doctrine filters

--- A2lixI18nDoctrineBundle.php
+++ A2lixI18nDoctrineBundle.php
@@ -14,5 +14,20 @@ class A2lixI18nDoctrineBundle extends Bundle
     {
         parent::build($container);
     }
-
+    
+    public function boot()
+    {
+        $em = $this->container->get('doctrine.orm.default_entity_manager');
+        $conf = $em->getConfiguration();
+        $conf->addFilter(
+            'oneLocale',
+            'A2lix\I18nDoctrineBundle\Doctrine\ORM\Filter\OneLocaleFilter'
+        );
+        $conf->addFilter(
+            'manyLocales',
+            'A2lix\I18nDoctrineBundle\Doctrine\ORM\Filter\ManyLocalesFilter'
+        );
+        $em->getFilters()->enable('oneLocale');
+        $em->getFilters()->enable('manyLocales');
+    }    
 }
